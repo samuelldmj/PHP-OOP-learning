a087b31367d0ee4e2d375f4931de912e7508c7b8
Samuel <samuelldmj5@gmail.com>
2023-11-10 09:53:42 +0100

    i learnt learnt late_static_binding

--- class_constants.php
+++ class_constants.php
@@ -1,5 +1,18 @@
 <?php 
+class clock{
+    public const DAY_IN_SECONDS = 60 * 60 * 24; 
 
+
+    public function tomorrow(){
+        return time() + self::DAY_IN_SECONDS;
+    }
+}
+
+echo clock::DAY_IN_SECONDS . "<br>";
+
+//insatnce of a class
+$clock = new clock;
+echo $clock->tomorrow()  . "<br>";
 
 
 
