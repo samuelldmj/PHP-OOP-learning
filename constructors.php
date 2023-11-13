4c22534142e036a90fbdc0793f97585153904d09
Samuel <samuelldmj5@gmail.com>
2023-11-13 14:23:06 +0100

    learnt constructor and destructor

---
+++ constructors.php
@@ -0,0 +1,52 @@
+<?php
+
+class Sofa {
+
+  public static $instance_count = 0;
+
+  public $seats = 3;
+  public $arms = 2;
+
+  //this call all the instances that was created
+  public function __construct($arg = []) {
+    self::$instance_count++;
+    $this->seats = $arg['seats'] ?? $this->seats;
+    $this->arms = $arg['arms'] ?? $this->arms;
+  }
+
+  public function __destruct(){
+    self::$instance_count--;
+  }
+
+}
+
+class Couch extends Sofa {
+  var $arms = 0;
+}
+
+class Loveseat extends Sofa {
+  var $seats = 2;
+}
+
+$sofa = new Sofa(['seat' => 3 , 'arms' => 2]);
+echo 'Sofa<br />';
+echo '- seats: ' . $sofa->seats . '<br />';
+echo '- arms: ' . $sofa->arms . '<br />';
+echo '<br />';
+
+$couch = new Couch(['seat' => 4]);
+echo 'Couch<br />';
+echo '- seats: ' . $couch->seats . '<br />';
+echo '- arms: ' . $couch->arms . '<br />';
+echo '<br />';
+
+unset($sofa);
+$loveseat = new Loveseat([ 'arms' => 0]);
+echo 'Loveseat<br />';
+echo '- seats: ' . $loveseat->seats . '<br />';
+echo '- arms: ' . $loveseat->arms . '<br />';
+echo '<br />';
+
+echo 'Instance count: ' . Sofa::$instance_count . '<br />';
+
+?>
