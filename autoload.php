865fffd4f41007c4aff616b77e8426793beacc2a
Samuel <samuelldmj5@gmail.com>
2023-11-23 10:12:56 +0100

    learnt how to use autoload in php

---
+++ autoload.php
@@ -0,0 +1,34 @@
+<?php
+
+function my_autoload($class){
+    if(preg_match('/\A\w+\Z/' , $class)){
+        include 'classes/'. $class . '.class.php';
+    }
+}
+    spl_autoload_register('my_autoload');
+    $bike = new Bicycle;
+    $bike->brand = "suzuki";
+    echo $bike->brand;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
