d16600f98633081bd2fc659472410a2bec0556b9
Samuel <samuelldmj5@gmail.com>
2023-12-18 17:02:38 +0100

    learnt namespacing concept

---
+++ Namespace/Tota.php
@@ -0,0 +1,20 @@
+<?php
+//Namespace
+//namespace is like putting classes in a group, which will enable me to make use of classes with similar name
+//you can't have two class with similar name it will throw an error
+
+
+require_once 'classA.php';
+require_once 'classB.php';
+
+//solution
+
+
+
+
+
+
+
+
+
+?>
---
+++ Namespace/classB.php
@@ -0,0 +1,14 @@
+<?php
+//Namespace
+
+//you can't have two class without similar name it will throw an error
+class order{
+    public function handle(){
+        echo "done!";
+    }
+    
+}
+
+
+
+?>
