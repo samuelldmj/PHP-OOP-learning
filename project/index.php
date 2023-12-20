9e9dbb71da591297f91a11c3a0b64f854f898756
Samuel <samuelldmj5@gmail.com>
2023-12-20 17:03:19 +0100

    Router project

---
+++ project/index.php
@@ -0,0 +1,24 @@
+<?php 
+//create a router class thats get the methods and paths we want to check for and the file it should require
+
+//.htaccess
+/*
+RewriteEngine On
+RewriteBase /
+RewriteCond %[REQUEST_FILENAME] !-d
+RewriteCond %[REQUEST_FILENAME] !-f
+RewriteRule ^(.+)$ index.php [QSA, L]
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+?>
