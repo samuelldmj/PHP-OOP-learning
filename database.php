d2a599e2409f48f965723660276e739a39b7bb07
Samuel <samuelldmj5@gmail.com>
2023-12-22 18:05:47 +0100

    database PDO

--- database.php
+++ database.php
@@ -8,9 +8,17 @@
 $pdo = new PDO(dsn: 'mysql:host=localhost;dbname=php_course', username:'root', password: '');
 
 //insert
-$count = $pdo->exec(statement: 'INSERT INTO sample (name) VALUES("samuel")' );
-echo "$count rows were affected";
+//the exec function can count the number of times a row was inserted and input a data in the db table
+// $count = $pdo->exec(statement: 'INSERT INTO sample (name) VALUES("samuel")' );
+//adding another value and counting the row
+$count = $pdo->exec(statement: 'INSERT INTO sample (name) VALUES("Kemi")' );
+//it get the count, and as well consider the last row inserted
+$lastid = $pdo->lastInsertId();
+//i can get the count again
+echo "$count rows were affected and the last inserted id is $lastid";
 
+//if we want to add multiple rows
+//we use the transaction function
 
 
 
